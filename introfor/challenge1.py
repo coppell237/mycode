13c91770aae16b5fb5b0ad37c381d30badcab3de
Jim Chang <coppell237@users.noreply.github.com>
2022-05-26 03:20:20 +0000

    studying for logic

---
+++ introfor/challenge1.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+farms = [{"name": "NE Farm", "agriculture": ["sheep", "cows", "pigs", "chickens", "llamas", "cats"]},
+         {"name": "W Farm", "agriculture": ["pigs", "chickens", "llamas"]},
+         {"name": "SE Farm", "agriculture": ["chickens", "carrots", "celery"]}]
+
+for farm in farms:
+    print(farm)     
+    print(farm.get("name", "Unknown Farm"), end=":\n")
+
+    for agri in farm.get("agriculture"):
+        print("   -", agri)
+
+print("\n----- End -----\n")
---
+++ introfor/forloop1.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+vendors = ["cisco", "juniper", "big_ip", "f5", "arista"]
+
+#loop across the list vendors
+
+for x in vendors:
+    print("The vendor is:" +x)     # looping until end of list
+
+print("\nOur loop has ended")
